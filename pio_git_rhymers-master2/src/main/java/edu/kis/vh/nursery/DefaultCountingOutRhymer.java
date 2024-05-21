4de5076a5cfb0f01e60f63f548717cf9c11bfbf7
WOJTASK1 <wojtek03.lep@gmail.com>
2024-05-21 21:23:08 +0200

    6.1 zmiana metody isFull-dodanie stałej zależenej do MAX_VALUE

--- pio_git_rhymers-master2/src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ pio_git_rhymers-master2/src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -18,7 +18,7 @@ public class DefaultCountingOutRhymer {
         }
         
             public boolean isFull() {
-                return total == 11;
+                return total == MAX_SIZE-1;
             }
         
                 protected int peekaboo() {
